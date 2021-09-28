a86239b6130cd35d0072e4b397f27fb576c9a1ef
kresocuk <57438862+kresocuk@users.noreply.github.com>
2021-09-28 13:29:00 +0200

    deleted files

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import PlayerData from "./Data.json";
 function App() {
  
   return (
-    // <Fragment>
-    //   <section>
-    //     <button onClick={fetchTeamsHandler}>Fetch Teams</button>
-    //   </section>
-    //   <section>
-    //     <TeamsList teams={teams} />
-    //   </section>
-    // </Fragment>
     <Fragment>
     <div className="App">
       <Search placeholder="Enter a name... " data={PlayerData} />
